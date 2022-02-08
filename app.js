003a7daf47fc583467b3c26d2dea89f35e702ba1
hyeonheebee <97822063+hyeonheebee@users.noreply.github.com>
2022-02-09 03:07:43 +0900

    1st informality assignment
    make caculator with vanilla JS

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-alert("hi");
+const caculator = { 
+    add: function(a,b){
+        console.log(a+b);
+    },
+    minus: function(a,b){
+        console.log(a-b);
+    },
+    divide: function(a,b){
+        console.log(a/b);
+    },
+    multi: function(a,b){
+        console.log(a*b);
+    },
+    squared: function(a,b){
+        console.log(a**b);
+    },
+    
+}
+
+caculator.add(1,2);
+caculator.minus(1,2);
+caculator.divide(1,2);
+caculator.multi(1,2);
+caculator.squared(-1,3);
+
