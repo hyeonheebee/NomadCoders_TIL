eaef2d6a1ef8bc5ea6a2b01d573df073393ab79c
hyeonheebee <97822063+hyeonheebee@users.noreply.github.com>
2022-03-19 01:48:34 +0900

    making Input_page shift
    dosen't work

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
+const inputForm = document.querySelector("#input-form");
 const nameForm = document.querySelector(".name-form");
 const nameInput = document.querySelector(".name-form input");
-
-const emailForm = document.querySelector(".name-form");
+const emailForm = document.querySelector(".email-form");
+const emailInput = document.querySelector(".email-form input");
+const emailSpan = document.querySelector(".email-form span");
 const pwForm = document.querySelector(".pw-form");
+const pwSpan = document.querySelector(".pw-form span");
 const pwText = document.querySelector("h4");
-
-function inputNameSubmit() {
-  const username = nameInput.value;
-
-  nameForm.classList.add("hidden");
-  console.log(username);
-  nameForm.classList.add("color");
+const HIDDEN_CLASSNAME = "hidden";
+function inputSubmit(event) {
+  if (nameInput.value !== "") {
+    const username = nameInput.value;
+    emailSpan.innerText = `${username}, What is your email?`;
+    event.preventDefault();
+    nameForm.classList.add(HIDDEN_CLASSNAME);
+    emailForm.classList.remove(HIDDEN_CLASSNAME);
+  } else if (emailInput.value !== "") {
+    const useremail = emailInput.value;
+    pwSpan.innerText = `${useremail} is that True? Please choose a password`;
+    emailForm.classList.add(HIDDEN_CLASSNAME);
+    nameForm.classList.add(HIDDEN_CLASSNAME);
+    pwForm.classList.remove(HIDDEN_CLASSNAME);
+  } else {
+    pwText.innerText = "PassWord need to be at least 8 characters long :)";
+  }
 }
 
-nameForm.addEventListener = ("submit", inputNameSubmit);
-
-function inputEmailSubmit(event) {
-  event.preventDefault();
-  emailForm.classList.add("hidden");
-}
-
-emailForm.addEventListener = ("submit", inputEmailSubmit);
-
-function inputPwSubmit(event) {
-  event.preventDefault();
-  pwForm.classList.add("hidden");
-}
-
-pwForm.addEventListener = ("submit", inputPwSubmit);
+inputForm.addEventListener("submit", inputSubmit);
