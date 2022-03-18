881d904d62f10bb6c0f78a9bf63bfcda2d31b5f7
hyeonheebee <97822063+hyeonheebee@users.noreply.github.com>
2022-03-19 02:01:01 +0900

    Making Input_innerFunction
    doesn't work

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const inputForm = document.querySelector("#input-form");
 const nameForm = document.querySelector(".name-form");
 const nameInput = document.querySelector(".name-form input");
 const emailForm = document.querySelector(".email-form");
@@ -8,22 +7,26 @@ const pwForm = document.querySelector(".pw-form");
 const pwSpan = document.querySelector(".pw-form span");
 const pwText = document.querySelector("h4");
 const HIDDEN_CLASSNAME = "hidden";
-function inputSubmit(event) {
-  if (nameInput.value !== "") {
-    const username = nameInput.value;
-    emailSpan.innerText = `${username}, What is your email?`;
-    event.preventDefault();
-    nameForm.classList.add(HIDDEN_CLASSNAME);
-    emailForm.classList.remove(HIDDEN_CLASSNAME);
-  } else if (emailInput.value !== "") {
+function inputNameSubmit(event) {
+  const username = nameInput.value;
+  emailSpan.innerText = `${username}, What is your email?`;
+  event.preventDefault();
+  nameForm.classList.add(HIDDEN_CLASSNAME);
+  emailForm.classList.remove(HIDDEN_CLASSNAME);
+
+  function inputEmailSubmit() {
     const useremail = emailInput.value;
     pwSpan.innerText = `${useremail} is that True? Please choose a password`;
     emailForm.classList.add(HIDDEN_CLASSNAME);
     nameForm.classList.add(HIDDEN_CLASSNAME);
     pwForm.classList.remove(HIDDEN_CLASSNAME);
-  } else {
-    pwText.innerText = "PassWord need to be at least 8 characters long :)";
+
+    function inputPwSubmit() {
+      pwText.innerText = "PassWord need to be at least 8 characters long :)";
+    }
+    pwForm.addEventListener("submit", inputPwSubmit);
   }
+  emailForm.addEventListener("submit", inputEmailSubmit);
 }
 
-inputForm.addEventListener("submit", inputSubmit);
+nameForm.addEventListener("submit", inputNameSubmit);
