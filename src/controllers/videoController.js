ce96b7a8f633d9ed452fdc337609d8250462b788
hyeonheebee <97822063+hyeonheebee@users.noreply.github.com>
2023-03-06 00:17:25 +0900

    removing # in hashtags(ing)
    browser console test is ok , but it doesn't work in my local

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -6,35 +6,71 @@ import Video from "../models/Video";
 export const home = async (req, res) => {
   const videos = await Video.find({}).sort({ createdDate: "desc" });
   const pageTitle = "home";
-  console.log(videos);
+
   return res.render("home", { pageTitle, videos });
 };
 export const search = (req, res) => {};
 export const see = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
-  console.log(video.genres);
+  if (!video) {
+    const errorMessage = "Sorry, we can't find any Video";
+    return res
+      .status(404)
+      .render("404", { pageTitle: "404 ERROR", errorMessage });
+  }
+  console.log(video);
   return res.render("video/see", { pageTitle: video.title, video });
 };
 export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
+  if (!video) {
+    const errorMessage = "Sorry, we can't find any Video";
+    return res
+      .status(404)
+      .render("404", { pageTitle: "404 ERROR", errorMessage });
+  }
   return res.render("video/edit", { pageTitle: "Edit Video", video });
 };
-export const postEdit = (req, res) => {};
+export const postEdit = async (req, res) => {
+  const { title, summary, genres, hashtags } = req.body;
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    const errorMessage = "Sorry, we can't find any Video";
+    return res
+      .status(404)
+      .render("404", { pageTitle: "404 ERROR", errorMessage });
+  }
+  await Video.findByIdAndUpdate(id, {
+    title,
+    summary,
+    hashtags: hashtags
+      .toString()
+      .split(",")
+      .map((x) => (x.startsWith("#") ? x : "#" + x)),
+    genres: genres.toString().split(","),
+  });
+  console.log(video);
+  return res.redirect("/");
+};
 export const getUpload = (req, res) => {
   return res.render("video/upload", { pageTitle: "upload" });
 };
 export const postUpload = async (req, res) => {
   const pageTitle = "Upload Video";
-  const { title, summary, genres } = req.body;
+  const { title, summary, genres, hashtags } = req.body;
 
   try {
     await Video.create({
       title,
       summary,
-      hashtags,
-      genres,
+      hashtags: hashtags
+        .toString()
+        .split(",")
+        .map((x) => (x.startsWith("#") ? x : "#" + x)),
+      genres: genres.toString().split(","),
     });
     return res.redirect("/");
   } catch (error) {
