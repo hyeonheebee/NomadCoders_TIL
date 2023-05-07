f7d1b50ef2daeb537704048c3809628ed625d0d8
hyeonheebee <97822063+hyeonheebee@users.noreply.github.com>
2023-05-08 01:06:52 +0900

    useEffect배우기
    useEffect를 통해 특정 코드만 실행, 특정코드를 원할때 실행, 서로 방해받지 않게 실행, 모듈러 원활하게

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,10 +1,31 @@
-import Button from "./Button";
+import { useState, useEffect } from "react";
+
 import styles from "./App.module.css";
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(() => console.log("i run only once"), []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("I run when 'keyword' changes and more than 5 word");
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'button' was clicked");
+  }, [counter]);
   return (
     <div>
-      <h1 className={styles.title}>welcome back!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>continue</button>
     </div>
   );
 }
