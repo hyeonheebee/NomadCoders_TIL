8d0f210aa662f3bcf0b801e37637f34935c16563
hyeonheebee <97822063+hyeonheebee@users.noreply.github.com>
2023-05-08 10:05:20 +0900

    clening up function
    return 에 원하는 함수를 넣는다

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,12 +1,22 @@
 import { useState, useEffect } from "react";
 
 import styles from "./App.module.css";
+
+function Hello() {
+  useEffect(() => {
+    console.log("created :)");
+    return () => console.log("destroyed :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
+  const [showing, setShowing] = useState(false);
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
-
+  const onShowing = () => setShowing((current) => !current);
   useEffect(() => console.log("i run only once"), []);
   useEffect(() => {
     if (keyword !== "" && keyword.length > 5) {
@@ -26,6 +36,9 @@ function App() {
       />
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>continue</button>
+
+      {showing ? <Hello /> : null}
+      <button onClick={onShowing}>{showing ? "Hide" : "Reveal"}</button>
     </div>
   );
 }
