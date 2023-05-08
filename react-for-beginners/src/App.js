152562d6ebcd1c3d396f68b0adb5dcda4fc98a7a
hyeonheebee <97822063+hyeonheebee@users.noreply.github.com>
2023-05-08 10:45:55 +0900

    간단한 todo list 를 만들기
    array에 새로운 요소 넣기, 새로운 array를 불러와서 list 로 html에 넣어주기

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,44 +1,38 @@
 import { useState, useEffect } from "react";
 
-import styles from "./App.module.css";
-
-function Hello() {
-  useEffect(() => {
-    console.log("created :)");
-    return () => console.log("destroyed :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  const onShowing = () => setShowing((current) => !current);
-  useEffect(() => console.log("i run only once"), []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("I run when 'keyword' changes and more than 5 word");
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
     }
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'button' was clicked");
-  }, [counter]);
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  useEffect(() => console.log(toDos), [toDos]);
+  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>continue</button>
-
-      {showing ? <Hello /> : null}
-      <button onClick={onShowing}>{showing ? "Hide" : "Reveal"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Wirte your todo"
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
