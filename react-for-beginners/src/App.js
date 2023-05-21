54cbc02e9aad46c74ced4718fd8139d90abfbfbc
hyeonheebee <97822063+hyeonheebee@users.noreply.github.com>
2023-05-22 00:13:54 +0900

    css 전,  react js 영화 앱 기능구현만 간단히
    css 전,  react js 영화 앱 기능구현만 간단히

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,51 +1,18 @@
-import { useState, useEffect } from "react";
-
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
-  const [loading, setloading] = useState(true);
-  const [coins, setCoins] = useState([]);
-
-  const [value, setValue] = useState(0);
-  const [index, setIndex] = useState(0);
-  const onSelect = (event) => {
-    setIndex(event.target.value);
-    console.log(event.target.value);
-  };
-  const onChange = (event) => setValue(event.target.value);
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => setCoins(json));
-    setloading(false);
-  }, []);
-
   return (
-    <div>
-      <h1>The coins! {coins.length} </h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <select value={index} onChange={onSelect}>
-        {coins.map((coin) => (
-          <option value={coin.name} key={coin.id}>
-            {coin.name}
-          </option>
-        ))}
-      </select>
-      {coins.map((coin) =>
-        coin?.name === index ? (
-          <span>
-            {coin.symbol} : {coin.quotes.USD.price} USD
-            <div>
-              <input value={value} onChange={onChange} />
-              <input
-                value={value / coin.quotes.USD.price}
-                onChange={onChange}
-              />
-              <span>{coin.name}</span>
-            </div>
-          </span>
-        ) : null
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
