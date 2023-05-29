3a91da8e30127e7bebb107ced44f88a7571560c3
Shim Hyeonhee <hyunheeshim0103@gmail.com>
2023-05-29 14:49:27 +0900

    commit temporairly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+const Father = styled.div`
+  display: flex;
+`;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const rotationAnimation = keyframes`
+from{
+  transform:rotate(0deg)
+}
+to{
+  transform:rotate(180deg)
 }
 
+`;
+const Hello = styled.h1`
+  font-size: 20px;
+  color: ${(props) => props.theme.textColor};
+`;
+const Wapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  background-color: ${(props) => props.theme.backgroundColor};
+  ${Hello}{
+    animation: :${rotationAnimation};
+  }
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+  span {
+    font-size: 50px;
+    &:hover {
+      font-size: 10px;
+    }
+    animation: ${rotationAnimation};
+  }
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+function App() {
+  return (
+    <Wapper>
+      <Hello />
+      <h1>hello</h1>
+      <Father>
+        <Box bgColor="blue">
+          <span>⭐</span>
+        </Box>
+        <Circle bgColor="teal" />
+      </Father>
+    </Wapper>
+  );
+}
 export default App;
