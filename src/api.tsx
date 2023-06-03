5617d69c4ac6194a057c78138a1b2b78334e362e
심현희_인천 <97822063+hyeonheebee@users.noreply.github.com>
2023-06-03 04:50:40 +0000

    "Initial commit"

---
+++ src/api.tsx
@@ -0,0 +1,8 @@
+const BASE_URL = "https://disney_api.nomadcoders.workers.dev/characters"
+
+export function fetchList () {
+ return fetch(`${BASE_URL}`).then(response => response.json())
+}
+export function fetchDetail(charId:number){
+  return fetch(`${BASE_URL}/${charId}`).then(response => response.json())
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import Home from "./Home";
+import Detail from "./screens/Detail";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([{
+path:"/",
+element:<App/>,
+children:[{
+  path:"",
+  element:<Home /> 
+},
+{path : "/character/:id",
+element:<Detail />}]
+}]);
 
 export default router;
